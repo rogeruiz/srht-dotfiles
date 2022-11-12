0e38026ddad06c9c399b59d44e45747bd0d3a10c
Roger Steve Ruiz <hi@rog.gr>
2022-11-12 00:09:38 -0500

    Getting completion engine working again in Neovim

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,21 +1,11 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-	return
+  return
 end
 
 local snippy_status_ok, snippy = pcall(require, "snippy")
 if not snippy_status_ok then
-	return
-end
-
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+  return
 end
 
 local icons = require("icons")
@@ -23,83 +13,72 @@ local icons = require("icons")
 local kind_icons = icons.kind
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			snippy.expand_snippet(args.body)
-		end,
-	},
-	mapping = {
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-e>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif snippy.can_expand_or_advance() then
-				cmp.complete()
-			elseif has_words_before() then
-				cmp.complete()
-			elseif check_backspace() then
-				fallback()
-			else
-				fallback()
-			end
-		end, { "i" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif snippy.can_jump(-1) then
-				snippy.previous()
-			else
-				fallback()
-			end
-		end, { "i" }),
-	},
-	formatting = {
-		fields = { "kind", "abbr", "menu" },
-		format = function(entry, vim_item)
-			-- Kind icons
-			vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-			-- NOTE: order matters
-			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				nvim_lua = "[Nvim]",
-				buffer = "[Buffer]",
-				path = "[Path]",
-				spell = "[Spell]",
-				emoji = "[Emoji]",
-				snippy = "[Snippet]",
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "buffer" },
-		{ name = "path" },
-		{ name = "spell" },
-		{ name = "emoji" },
-		{ name = "snippy" },
-	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	view = {
-		entries = "native",
-	},
-	experimental = {
-		ghost_text = true,
-	},
+  snippet = {
+    expand = function(args)
+      snippy.expand_snippet(args.body)
+    end,
+  },
+  experimental = {
+    ghost_text = true,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- NOTE: order matters
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[Nvim]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+        spell = "[Spell]",
+        emoji = "[Emoji]",
+        snippy = "[Snippet]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "buffer" },
+    { name = "path" },
+    { name = "spell" },
+    { name = "emoji" },
+    { name = "snippy" },
+  }, {
+    { name = "buffer" },
+  }),
+
+  cmp.setup.cmdline({ "/", "?" }),
+  {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  },
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      { name = "cmdline" },
+    }),
+  }),
 })
