c0733ecc4f1a660e1c9a841e682fb3fde45e5a18
Roger Steve Ruiz <hi@rog.gr>
2023-01-29 13:09:34 -0500

    Fixing sound plugin;
    The sound.sh plugin wasn't working properly when I connect the audio
    source to something that doesn't allow the Mac to control the audio.
    This happens when using the HDMI audio out. So I'm using a little TV
    icon along with an HDMI input icon to make it known that the audio can't
    be controlled by the Mac for that specific output.

--- private_dot_config/sketchybar/plugins/executable_sound.sh
+++ private_dot_config/sketchybar/plugins/executable_sound.sh
@@ -3,24 +3,31 @@
 VOLUME=$(osascript -e "output volume of (get volume settings)")
 MUTED=$(osascript -e "output muted of (get volume settings)")
 
-if [[ $MUTED != "false" ]]; then
-ICON="ﱝ"
+if [[ $VOLUME == "missing value" ]]
+then
+  ICON="﴿"
+  VOLUME=""
 else
-case ${VOLUME} in
-  100) ICON="";;
-  9[0-9]) ICON="";;
-  8[0-9]) ICON="";;
-  7[0-9]) ICON="";;
-  6[0-9]) ICON="";;
-  5[0-9]) ICON="";;
-  4[0-9]) ICON="";;
-  3[0-9]) ICON="";;
-  2[0-9]) ICON="";;
-  1[0-9]) ICON="";;
-  [0-9]) ICON="";;
-  *) ICON=""
-esac
+  if [[ $MUTED != "false" ]]; then
+    ICON="ﱝ"
+  else
+    case ${VOLUME} in
+      100) ICON="";;
+      9[0-9]) ICON="";;
+      8[0-9]) ICON="";;
+      7[0-9]) ICON="";;
+      6[0-9]) ICON="";;
+      5[0-9]) ICON="";;
+      4[0-9]) ICON="";;
+      3[0-9]) ICON="";;
+      2[0-9]) ICON="";;
+      1[0-9]) ICON="";;
+      [0-9]) ICON="";;
+      *) ICON=""
+    esac
+    VOLUME="${VOLUME}%"
+  fi
 fi
 
 sketchybar --set $NAME icon=$ICON \
-           --set $NAME label="$VOLUME%"
+           --set $NAME label="$VOLUME"
