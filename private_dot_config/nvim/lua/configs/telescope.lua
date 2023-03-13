a7ee363c575776a0bbf03517ca71411448aeda89
Roger Steve Ruiz <hi@rog.gr>
2023-03-13 16:39:05 -0400

    Actualizando a Telescope

--- private_dot_config/nvim/lua/configs/telescope.lua
+++ private_dot_config/nvim/lua/configs/telescope.lua
@@ -145,8 +145,9 @@ telescope.setup({
 -- Key mappings
 vim.cmd([[
 nnoremap <C-p> <cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>
-nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+nnoremap <C-i> <cmd>lua require('telescope.builtin').resume()<cr>
+nnoremap <leader>bt <cmd>lua require('telescope.builtin').live_grep({hidden=true})<cr>
+nnoremap <leader>bb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>ba <cmd>lua require('telescope.builtin').help_tags()<cr>
 nnoremap <leader>gs <cmd>lua require('telescope.builtin').git_status()<cr>
 ]])
