b41d5330449cbbe40b07ccc596bb3fd93e78de9c
Roger Steve Ruiz <hi@rog.gr>
2023-07-09 13:11:01 -0400

    Cambiando la visualización WiFi;
    Este cambieno ya usa la tasa de TX (transmisión) de la conexión de WiFi.

--- private_dot_config/sketchybar/plugins/executable_wifi.sh
+++ private_dot_config/sketchybar/plugins/executable_wifi.sh
@@ -20,19 +20,46 @@ CURR_TX="$(
   rg -o "lastTxRate: .*" | \
   sed 's/^lastTxRate: //'
 )"
+MAX_TX="$(
+  echo "$CURRENT_WIFI" |
+  rg -o "maxRate: .*" | \
+  sed 's/^maxRate: //'
+)"
+
 
 # Y al final porque la tasa esta escrita en Megabits la vamos a convertir en
 # Gigabits porque me gusta mejor así. Yo consumo mucho Internet entonces me
 # gusta ver un número más preciso.
 # @desempeño 0.012 segundos
-CURR_TX=$(
-  echo "scale=3; $CURR_TX / 1000" | \
-  bc | \
-  awk '{ printf "%.3f", $0 }'
+CURR_CALC=$(
+  echo "scale=2; $CURR_TX / $MAX_TX * 100" | bc | cut -d '.' -f 1
 )
+
+# Si la calculación no se aplicar correctamente, se defina la fuerza ha ciento
+# porcento pa' que salge algo en la barrilla.
+CURR_FUERZA="󰤩"
+
+# Calculemos el porcentaje de la señal usand el TX max y el TX currente.
+# Empezemos de cero
+if (( 0 <= $CURR_CALC && $CURR_CALC <= 20))
+then
+  CURR_FUERZA="󰤯"
+elif (( 21 <= $CURR_CALC && $CURR_CALC <= 40))
+then
+  CURR_FUERZA="󰤟"
+elif (( 41 <= $CURR_CALC && $CURR_CALC <= 60))
+then
+  CURR_FUERZA="󰤢"
+elif (( 61 <= $CURR_CALC && $CURR_CALC <= 80))
+then
+  CURR_FUERZA="󰤥"
+elif (( 81 <= $CURR_CALC && $CURR_CALC <= 100))
+then
+  CURR_FUERZA="󰤨"
+fi
 
 if [[ -z "$SSID" ]]; then
   sketchybar --set $NAME label.drawing=off icon=睊
 else
-  sketchybar --set $NAME label.drawing=on label="$SSID (${CURR_TX} Gb/s)" icon=直
+  sketchybar --set $NAME label.drawing=on label="$SSID ${CURR_FUERZA}" icon=直
 fi
