b9590cba465760e6925f29bbdc312ebba952c65f
Roger Steve Ruiz <hi@rog.gr>
2023-07-09 13:04:27 -0400

    Cambiando como muestro la aplicaciónes del frente

--- private_dot_config/sketchybar/plugins/executable_front_app.sh
+++ private_dot_config/sketchybar/plugins/executable_front_app.sh
@@ -5,4 +5,44 @@
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
+case $INFO in
+  "1Password") INFO="󰟵" ;;
+  "Adobe Photoshop 2023") INFO="" ;;
+  "Asistente de Redes Captivas") INFO="󰀂" ;;
+  "Alacritty") INFO="" ;;
+  "Amphetamine") INFO="" ;;
+  "Backblaze") INFO="" ;;
+  "Bitdefender Endpoint Security Tools") INFO="󰒃" ;;
+  "Calculadora") INFO="󰪚" ;;
+  "Calendario") INFO="󰧒" ;;
+  "Configuración del Sistema") INFO="" ;;
+  "Creative Cloud") INFO="" ;;
+  "Docker") INFO="󰡨" ;;
+  "Finder") INFO="󰀶" ;;
+  "Firefox") INFO="󰈹" ;;
+  "Google Chrome") INFO="󰊯" ;;
+  "iA Writer") INFO="󰴒" ;;
+  "Keybase") INFO="󱕵" ;;
+  "KeyCastr") INFO="" ;;
+  "LaunchBar") INFO="󰌓" ;;
+  "loginwindow") INFO="󰍂" ;;
+  "Mail") INFO="󰛮" ;;
+  "Menú de Kandji") INFO="󰾡" ;;
+  "Miro") INFO="󱁉" ;;
+  "Música") INFO="" ;;
+  "Monitor de Actividad") INFO="" ;;
+  "Notational Velocity") INFO="" ;;
+  "osascript") INFO="󰯁" ;;
+  "Photo Booth") INFO="" ;;
+  "Safari") INFO="󰀹" ;;
+  "Slack") INFO="󰒱" ;;
+  "Spotify") INFO="󰓇" ;;
+  "Thunderbird") INFO="󰴃" ;;
+  "Velja") INFO="󰄛" ;;
+  "Vista Previa") INFO="" ;;
+  "zoom.us") INFO="󰨜" ;;
+  *)
+    ;;
+esac
+
 sketchybar --set $NAME label="${INFO:= }"
